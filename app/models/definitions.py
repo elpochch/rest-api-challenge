eaa8d5e86bfe43d7fbd7f72c1ded61a4eaa2f61d
Brayan Garzon <brayan.villabon@gmail.com>
2025-02-05 04:23:29 -0300

    refactor schema definitions

--- app/models/definitions.py
+++ app/models/definitions.py
@@ -1,25 +1,32 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
-class HiredEmployees(BaseModel):
+Base = declarative_base()
+
+class HiredEmployees(Base):
     """
     #TODO: Add description
     """
-    id: int
-    name: str
-    datetime: str
-    department_id: int
-    job_id: int
+    __tablename__ = "hired_employees"
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', String, nullable=False)
+    datetime = Column('datetime', String, nullable=False)
+    department_id = Column('department_id', Integer, nullable=False)
+    job_id = Column('job_id', Integer, nullable=False)
 
-class Departments(BaseModel):
+class Departments(Base):
     """
     :TODO: Add description
     """
-    id: int
-    department: str
+    __tablename__ = "departments"
+    id = Column('id',Integer, primary_key=True)
+    department = Column('department', String, nullable=False)
 
-class Jobs(BaseModel):
+class Jobs(Base):
     """
     :TODO Add description
     """
-    id: int
-    job: str
+    __tablename__ = "jobs"
+    id = Column('id', Integer, primary_key=True)
+    job = Column('job', String, nullable=False)
